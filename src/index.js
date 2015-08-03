23195ae8c4329c270fafca5d5cc5964c1fa18c06
Gary Soedarsono <garysoed@gmail.com>
2015-08-02 21:34:51 -0700

    fixed caching bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import Binding from './binding';
-import RunContext from './runcontext';
 import Scope from './scope';
 
 ((window) => {
   window['DIJS'] = new Scope();
   window['DIJS']['Scope'] = Scope;
-  window['DIJS']['Binding'] = Binding;
-  window['DIJS']['RunContext'] = RunContext;
 })(window);
--- src/scope.js
+++ src/scope.js
@@ -1,128 +1,99 @@
-import Binding from './binding';
-import RunContext from './runcontext';
-
-// Private symbols.
-const __localBindings__ = Symbol('localBindings');
+const __globalBindings__ = Symbol('globalBindings');
+const __key__ = Symbol('key');
 const __parentScope__ = Symbol('parentScope');
-const __prefix__ = Symbol('prefix');
-const __rootScope__ = Symbol('rootScope');
-
-const __addBinding__ = Symbol('registerProvider');
-const __findProvider__ = Symbol('findProvider');
+const __provider__ = Symbol('provider');
+const __searchAncestor__ = Symbol('searchAncestor');
 
 class Scope {
-  /**
-   * Scope containing local bindings.
-   *
-   * @class DI.Scope
-   * @constructor
-   * @param {DI.Scope} [parentScope=null] The parent scope.
-   */
-  constructor(parentScope = null, rootScope = this) {
-    this[__localBindings__] = new Map();
+  constructor(provider = null, key = '(root)', parentScope = null, globalBindings = new Map()) {
+    this[__globalBindings__] = globalBindings;
+    this[__key__] = key;
     this[__parentScope__] = parentScope;
-    this[__rootScope__] = rootScope;
+    this[__provider__] = provider;
   }
 
-  [__addBinding__](key, binding) {
-    if (this[__localBindings__].has(key)) {
-      throw new Error(`${key} is already bound`);
-    }
-    this[__localBindings__].set(key, binding);
-  }
-
-  findBinding(key) {
-    // Checks the local binding.
-    let binding = this[__localBindings__].get(key);
-    if (binding === undefined) {
-      if (this[__parentScope__]) {
-        return this[__parentScope__].findBinding(key);
-      } else {
-        return undefined;
-      }
+  [__searchAncestor__](key) {
+    if (this[__key__] === key) {
+      return this;
+    } else if (this[__parentScope__]){
+      return this[__parentScope__][__searchAncestor__](key);
     } else {
-      return binding;
+      return undefined;
     }
   }
 
-  /**
-   * Creates a new child scope with the given value bound to the given key in its local binding.
-   *
-   * TODO(gs)
-   *
-   * @method with
-   * @param {string} key The key to bound the value to.
-   * @param {Object} keys Object with mapping of variable name to the bound name.
-   * @param {Function} fn The provider function to run.
-   * @return {DI.Scope} The newly created child scope.
-   */
-  with(key, fn) {
-    let childScope = new Scope(this, this[__rootScope__]);
-    let binding = new Binding(key, fn, childScope);
-    childScope[__addBinding__](key, binding);
-    return childScope;
+  with(key, provider) {
+    return new Scope(provider, key, this, this[__globalBindings__]);
   }
 
-  /**
-   * Creates a new child scope with the given value bound to the given key in its local binding.
-   * This is similar to {{#crossLink "DI.Scope/with"}}{{/crossLink}}, but the value is a constant.
-   *
-   * @method constant
-   * @param {string} key The key to bound the value to.
-   * @param {Object} value The object to bind to the given key.
-   * @return {DI.Scope} The newly created child scope.
-   */
   constant(key, value) {
     return this.with(key, () => value);
   }
 
-  /**
-   * Binds the given value to the given key. The execution scope of the provider function is still
-   * this scope.
-   *
-   * TODO(gs)
-   *
-   * @method bind
-   * @param {string} key The key to bound the value to.
-   * @param {Object} keys Object with mapping of variable name to the bound name.
-   * @param {Function} fn The provider function to run.
-   */
   bind(key, fn) {
-    let binding = new Binding(key, fn, this);
-    this[__rootScope__][__addBinding__](key, binding);
+    let newScope = this.with(key, fn);
+    if (this[__globalBindings__].has(key)) {
+      throw new Error('Key ${key} is already bound');
+    }
+    this[__globalBindings__].set(key, newScope);
     return this;
   }
 
-  /**
-   * Runs the given function after injecting any dependencies.
-   *
-   * TODO(gs)
-   *
-   * @method run
-   * @param {Function} fn The function to run.
-   */
   run(fn) {
-    let runBinding = new Binding(null, fn, this);
+    let runScope = this.with('(run)', fn);
+    return runScope.resolve('(run)');
+  }
 
-    // Resolves all the bindings in the current scope.
-    let resolvedValues = new RunContext();
+  resolve(key, runScope = this, runContext = new Map(), searchChain = []) {
+    // Check if the key is already in the search chain.
+    if (searchChain.indexOf(key) >= 0) {
+      throw new Error(`Cyclic dependency:\n${searchChain.join(' -> ')} -> ${key}`);
+    }
 
-    let resolveBindings = function(scope) {
-      for (let [key, binding] of scope[__localBindings__]) {
-        binding.resolve(resolvedValues);
-      }
+    let childSearchChain = searchChain.concat([key]);
 
-      if (scope[__parentScope__]) {
-        resolveBindings(scope[__parentScope__]);
-      }
-    };
+    // First, find the ancestral scope.
+    let scope = this[__searchAncestor__](key);
 
-    resolveBindings(this);
-    return runBinding.resolve(resolvedValues);
+    // Second, find in the running scope.
+    if (scope === undefined) {
+      scope = runScope[__searchAncestor__](key);
+    }
+
+    // Finally, search in the global bindings.
+    if (scope === undefined && this[__globalBindings__].has(key)) {
+      scope = this[__globalBindings__].get(key);
+    }
+
+    if (scope === undefined) {
+      return undefined;
+    }
+
+    if (!runContext.has(scope)) {
+      let optional = key => {
+        return scope.resolve(key, runScope, runContext, childSearchChain);
+      };
+      let require = key => {
+        let value = optional(key);
+        if (value === undefined) {
+          throw new Error(`Cannot find ${key}:\n${searchChain.join(' -> ')} -> ${key}`);
+        }
+        return value;
+      };
+
+      runContext.set(scope, scope[__provider__](require, optional));
+    }
+
+    return runContext.get(scope);
+  }
+
+  toString() {
+    let parentStrPart = this[__parentScope__] ? [this[__parentScope__].toString()] : [];
+    return [this[__key__]].concat(parentStrPart).join(' -> ');
   }
 
   reset() {
-    this[__localBindings__].clear();
+    this[__globalBindings__].clear();
   }
 }
 
